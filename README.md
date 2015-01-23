81f4cc384e9245e990bc3be48fe9cf6e0e30a890
Hans Hasselberg <me@hans.io>
2015-01-23 11:38:03 +0100

    :sunrise:

---
+++ README.md
@@ -0,0 +1,14 @@
+# onelinejson
+
+A Clojure library designed to log one line of json for each request.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2015 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/onelinejson/core.clj
@@ -0,0 +1,22 @@
+(ns onelinejson.core)
+
+(defn wrap-with-logger [handler]
+  (fn [request]
+    (let [start (System/currentTimeMillis)]
+      (let [response (handler request)
+            finish (System/currentTimeMillis)
+            duration  (- finish start)
+            data {
+                  "debug_info" {}
+                  "response" {
+                              "status" (get response :status)
+                              "duration" duration}
+                  "request" {
+                             "method" (get request :request-method)
+                             "path" (get request :uri)
+                             "headers" (get request :headers)
+                             "params" (get request :query-string)
+                             "ip" (get request :remote-addr)}
+                  }]
+        println (json/write-str data)
+        response))))
